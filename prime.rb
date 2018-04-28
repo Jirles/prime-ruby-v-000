6189f90cd142dbda535aeb2bf37e3c4ee491604e
Emily Jirles <emily.jirles@gmail.com>
2018-04-28 13:53:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,13 +4,21 @@ def prime?(n)
   bool_hash = {}
   if n <= 1 
     prime = false
+  #elsif n == 2 
+    #prime = true
   else
     (2..n).each{|number| bool_hash[number] = true}
     bool_hash.each do |key, value|
       if value
-        
+        p_value = key
+        bool_hash.each do |key|
+          if key % p_value == 0 
+            bool_hash[key] = false
+          end
+        end
+      end
     end
-    
+    bool_hash[n] ? prime = true : prime = false
   end
   prime 
 end
