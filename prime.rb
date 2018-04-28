8b81a94c0fd2362ebf1a50e3ed73c064d89d0bb1
Emily Jirles <emily.jirles@gmail.com>
2018-04-28 13:21:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(n)
+  baby_primes = [2, 3, 5, 7]
   if n <= 1 
     prime = false
+  elsif baby_primes.include?(n) 
+    prime = true 
   else
     range = (2..n).to_a
     
