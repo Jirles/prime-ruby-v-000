e03fa1c91f21e04f1f3c4574d0dd9dd4821e79ce
Emily Jirles <emily.jirles@gmail.com>
2018-04-28 13:16:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,9 @@
 # Add  code here!
 def prime?(num)
-  prime = false
   if num <= 1 
-    
-  elsif || PRIMES.include?(num)
-    prime = true 
+    prime = false
   else
-    num_bool = PRIMES.collect{|prime| num % prime == 0}
-    prime = true if num_bool.all?(false)
-    PRIMES << num
+    
   end
   prime 
 end
