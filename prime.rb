0d5348966dbc88c8047f5b6dc194287758fe091f
Emily Jirles <emily.jirles@gmail.com>
2018-04-28 12:46:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+primes = [2]
+def prime?(num)
+  prime = false
+  if num == 1 || num in primes
+    prime = true 
+  else
+    primes.each do |prime|
+      num % prime == 0
+    
+end
